6e09abb27bcf6837974c8bcd8f2281adb3bbb018
Kate <kbalducci@hotmail.com>
2015-03-11 15:21:53 -0500

    added acts

--- app/controllers/acts_controller.rb
+++ app/controllers/acts_controller.rb
@@ -1,2 +1,27 @@
 class ActsController < ApplicationController
+
+  def index
+    @acts = Act.all
+  end
+
+  def show
+    @act = Act.find(params[:id])
+  end
+
+  def create
+    @act = Act.new(act_params)
+
+    respond_to do |format|
+      if @act.save
+        format.html { redirect_to @act, notice: 'User was successfully created.' }
+      else
+        format.html { render "welcome/index", notice: "There was a problem." }
+      end
+    end
+  end
+
+  private
+  def act_params
+    params.require(:act).permit(:title)
+  end
 end
--- app/models/act.rb
+++ app/models/act.rb
@@ -5,20 +5,4 @@ class Act < ActiveRecord::Base
   has_many :tags, through: :taggings
   accepts_nested_attributes_for :tags
 
-  def create
-    @act = Act.new(act_params)
-
-    respond_to do |format|
-      if @act.save
-        format.html { redirect_to @act, notice: 'User was successfully created.' }
-      else
-        format.html { render "welcome/index", notice: "There was a problem." }
-      end
-    end
-  end
-
-  private
-  def act_params
-    params.require(:act).permit(:title)
-  end
 end
