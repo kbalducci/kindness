fbd1cab102c4b7a0f15637268a166e1bebab37a4
Kate <kbalducci@hotmail.com>
2015-03-10 22:40:21 -0500

    updated notices

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
-        format.html { render "welcome/index" }
+        format.html { render "welcome/index", notice: "There was a problem." }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
