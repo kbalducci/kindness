5828067e383fe48aa8e802e895478f78c3590413
Kate <kbalducci@hotmail.com>
2015-03-21 21:13:37 -0500

    updated zipcode search and reset functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,11 +16,11 @@ class User < ActiveRecord::Base
 
 
   has_attached_file :avatar,
-  :styles => { :medium => "300x300>", :thumb => "100x100>" },
-  :bucket => 'kbjsinstaclone',
-  :url =>':s3_domain_url',
-  :path => ':class/:attachment/:id_partition/:style/:filename',
-  :default_url => "/images/:style/missing.png"
+                    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :bucket => 'kbjsinstaclone',
+                    :url =>':s3_domain_url',
+                    :path => ':class/:attachment/:id_partition/:style/:filename',
+                    :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   def kindships_finished
